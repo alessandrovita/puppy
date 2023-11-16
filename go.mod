99856a731296cb18a931409714d39fd8f32d3976
Alessandro Vita <al.vita@reply.it>
2023-11-16 15:47:49 +0100

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alessandrovita/puppy
 
 go 1.21.3
+
+require github.com/alessandrovita/dog v0.0.0-20231116143606-e9a5aabff648 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/alessandrovita/dog"
+)
+
 func Bark() string {
 	return "Woaf"
 }
 
 func Barks() string {
 	return "Woaf Woaf!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
